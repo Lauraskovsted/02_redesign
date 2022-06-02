e8572a18b38a0f13dfd790c062be11fb5911c90b
fili5863​ <fili5863@stud.kea.dk>
2022-06-02 15:00:39 +0200

    video virker med knap

---
+++ js/index.js
@@ -0,0 +1,25 @@
+const vidContainer = document.querySelector(".video-player");
+const vid = document.querySelector(".video-player video");
+const btn = document.querySelector(".video-player-button");
+
+function togglePlay() {
+  if (vid.paused) {
+    vid.play();
+    vidContainer.classList.add("playing");
+    btn.textContent = "Pause";
+  } else {
+    vid.pause();
+    btn.textContent = "Play";
+    vidContainer.classList.remove("playing");
+  }
+}
+
+function videoEnded() {
+  if (vidContainer.classList.contains("playing")) {
+    vidContainer.classList.remove("playing");
+  }
+  btn.textContent = "Play";
+}
+
+btn.addEventListener("click", togglePlay);
+vid.addEventListener("ended", videoEnded);
